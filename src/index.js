3e5819e284ea61b0c7699b96f5707576282b2507
WalterWit <walter_wit@hotmail.com>
2022-02-23 14:46:44 -0300

    c

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+//1) Tener la variable React en el scope
 import React from 'react';
+//const React = require('react');
+
+//Creame una variable llamada React en este archivo index.js que tenga todo lo que hay en la libreria react en su archivo index.js
+
+//2) Tener la variable ReactDOM en el scope
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//3) Tener un componente de React
+function App () {
+    return "Hola Mundo"
+}
+
+
+//4) Renderizar App en el DOM 
+ReactDOM.render(App(),document.getElementById('root'))
