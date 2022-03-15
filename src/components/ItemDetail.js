374acecca992929069a52eab8fb796f64230ea56
WalterWit <walter_wit@hotmail.com>
2022-03-14 23:30:31 -0300

    Detalle de Producto

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,18 +1,21 @@
 import React, { Fragment } from 'react'
 
 export const ItemDetail = ({productos}) => {
-    let [producto] = productos
+    const [producto] = productos
     return (
         <Fragment>
             {
-            producto ?
-            <h2>
-                {producto[0].nombre}
-            </h2>
-            :
-            <p className='itemDetail'>
-                {producto[0].det}
-            </p>
+                producto ?
+                <>
+                    <h3>
+                        {productos[0].nombre}
+                    </h3>
+                    <p className='itemDetail'>
+                        {productos[0].det}
+                    </p>
+                </>
+                :
+                <p>Cargando...</p>
             }
         </Fragment>
     )
