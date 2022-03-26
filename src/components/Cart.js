78f480547fe79b2edce02da39403ffd17ccf9628
WalterWit <walter_wit@hotmail.com>
2022-03-26 01:23:09 -0300

    No se que rompi

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,20 @@
-import {React, useContext} from 'react'
-import { nube } from './CartContext'
+import { React, useContext } from 'react'
+import { CartContext } from './CartContext'
 
 export const Cart = () => {
-    const ItemsCarrito = useContext(nube)
-    console.log(ItemsCarrito)
+    const {carrito, vaciar, quitarItem} = useContext(CartContext)
     return (
-        <div>
+        <>
             <h2>Carrito</h2>
-            <p>{ItemsCarrito[1]}</p>
-        </div>
+            <button onClick={vaciar}>Vaciar carrito</button>
+                {carrito.map(carrito =>{
+                    <>
+                        <h2>{carrito.producto.nombre}</h2>
+                        <img src={carrito.producto.imagen}/>
+                        <button onClick={quitarItem}>Quitar producto</button>
+                        {console.log('carrito:', carrito)}
+                    </>
+                })}
+        </>
     )
 }
